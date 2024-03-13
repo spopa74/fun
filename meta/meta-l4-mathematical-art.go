dc0b8c016bafc63fe84ad0e8a207986763457a9f
spopa74 <spopa74@gmail.com>
2024-03-13 16:24:41 -0400

    comment out for now

---
+++ meta/meta-l4-mathematical-art.go
@@ -0,0 +1,44 @@
+package meta
+
+// IN PROGRESS
+
+// STRUCTS
+// type plus struct {
+// 	x, y int32
+// }
+// type lattice struct {
+// 	x, y    int32
+// 	hasPlus bool
+// }
+// type DStroke struct {
+// 	x            int32
+// 	startY, endY int32
+// 	direction    int32 // -1 if down to up, +1 if up to down
+// }
+// type RStroke struct {
+// 	startX, endX int32
+// 	y            int32
+// 	direction    int32 // -1 if right to left, +1 if left to right
+// }
+
+// we need a struct which allows us to immediately figure out if there's
+// an intersection between 2 segments, conditions:
+// let's say we have an D segment coming, we need to:
+// - filter out quickly which
+//
+
+// VARS
+// var pluses []plus = []plus{} // empty
+// var currentX, currentY int32 // init at 0,0
+// // var leftRights []leftRightStroke // direction doesn't really matter
+// // var upDowns []upDownStroke       // direction doesn't really matter
+
+// func getPlusSignCount(N int32, L []int32, D string) int64 {
+// 	// Write your code here
+
+// 	return 0
+// }
+
+// func doesIntersect() {
+
+// }
